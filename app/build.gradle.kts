1102a5e33920b563eb9267b4f7b0e17977eb939f
Mirus <120586399+mirusmeta@users.noreply.github.com>
2025-02-10 00:34:04 +0300

    changed version of beta app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 25
         targetSdk = 33
         versionCode = 2
-        versionName = "1.1.2"
+        versionName = "1.1.2-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
