b96f631306f68fd04b074b064762a543d72b82f4
Mirus <120586399+mirusmeta@users.noreply.github.com>
2025-02-12 20:02:41 +0300

    Избавление ошибок в onBackPressed()

--- app/src/main/java/ru/mirus/MainActivity.kt
+++ app/src/main/java/ru/mirus/MainActivity.kt
@@ -573,6 +573,8 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    @Deprecated("This method has been deprecated in favor of using the\n      {@link OnBackPressedDispatcher} via {@link #getOnBackPressedDispatcher()}.\n      The OnBackPressedDispatcher controls how back button events are dispatched\n      to one or more {@link OnBackPressedCallback} objects.")
+    @SuppressLint("MissingSuperCall")
     override fun onBackPressed() {
 
     }
